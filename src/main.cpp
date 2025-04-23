26b9f40bb5ef7f84cd809bf264dc2d807f9fd4c4
Marco Sanvito <marco.sanvito@hotmail.com>
2025-04-23 14:31:03 +0200

    feat: used intel mkl and added cmake build for windows

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 #include <filesystem>
 // #include <Eigen/SparseCholesky>
 #include <Eigen/CholmodSupport>
+#ifdef EIGEN_USE_MKL_ALL
+#include <mkl.h>
+#endif
+
+typedef Eigen::SparseMatrix<double, Eigen::ColMajor, SuiteSparse_long> SparseMatrix;
+//typedef Eigen::SparseMatrix<double> SparseMatrix;
 
 constexpr auto HEADER_CSV = "timestamp,matrixname,rows,cols,nonZeros,loadTime,loadMem,decompTime,decompMem,solveTime,solveMem,error"; 
 constexpr auto OUT_FILE = "bench.csv";
@@ -19,8 +25,10 @@ int main(int argc, char* argv[], char** envp) {
 #ifdef EIGEN_VECTORIZE
   std::cerr << "Eigen CPU vectorization is enabled." << std::endl;
 #endif
-  Eigen::setNbThreads(0);
   std::cerr << "Eigen Num of Threads: " << Eigen::nbThreads() << std::endl;
+#ifdef EIGEN_USE_MKL_ALL
+  std::cerr << "MKL max threads: " << mkl_get_max_threads() << std::endl;
+#endif
 
   const std::filesystem::path matrices_dir = "matrices";
 
@@ -28,14 +36,12 @@ int main(int argc, char* argv[], char** envp) {
   for (const auto& entry : std::filesystem::directory_iterator(matrices_dir)) {
     if (entry.is_regular_file() && entry.path().extension() == ".mtx") {
       const auto matrixName { entry.path().stem().string() };
-
-      std::cerr << "Processing " << matrixName << "..." << std::endl;
       
       int result = solveMatrixMarket(entry.path());
       if (result == 0) {
-        std::cerr << "Processed " << matrixName << "..." << std::endl;
+        std::cerr << "Processed " << matrixName << std::endl;
       } else {
-        std::cerr << "Error " << matrixName << "..." << std::endl;
+        std::cerr << "Error " << matrixName << std::endl;
       }
     }
   }
@@ -46,6 +52,8 @@ int main(int argc, char* argv[], char** envp) {
 int solveMatrixMarket(const std::filesystem::path& path) {
   const auto timestamp = std::chrono::system_clock::now();
 
+  std::cerr << std::format("{} - Processing {}...", timestamp, path.stem().string()) << std::endl;
+
   std::ifstream matrix_file(path, std::ios::in);
   std::ofstream csv_file(OUT_FILE, std::ios::ate | std::ios::app);
 
@@ -55,7 +63,9 @@ int solveMatrixMarket(const std::filesystem::path& path) {
     csv_file.flush();
   }
 
-  Eigen::SparseMatrix<double> A;
+  SparseMatrix A;
+
+  std::cerr << std::format("Reading matrix...") << std::endl;
 
   auto start{ std::chrono::high_resolution_clock::now() };
   fast_matrix_market::read_matrix_market_eigen(matrix_file, A);
@@ -67,18 +77,19 @@ int solveMatrixMarket(const std::filesystem::path& path) {
   const auto loadTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
   const auto loadMem = 0;
 
-  csv_file << std::format("{}, {},", loadTime, loadMem);
+  std::cerr << std::format("Matrix read took {} ms and {} bytes", loadTime, loadMem) << std::endl;
+  csv_file << std::format("{}, {}, ", loadTime, loadMem);
 
-  Eigen::CholmodSimplicialLLT<Eigen::SparseMatrix<double>> solver;
+  Eigen::CholmodDecomposition<SparseMatrix> solver;
 
+  // Make sure the matrix is in compressed column form
+  //A.makeCompressed();
+
+  std::cerr << std::format("Decomposing matrix...") << std::endl;
   start = std::chrono::high_resolution_clock::now();
   solver.compute(A);
   end = std::chrono::high_resolution_clock::now();
-
-  const auto decompTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-  const auto decompMem = 0;
-  csv_file << std::format("{}, {},", decompTime, decompMem);
-
+  
   if (solver.info() != Eigen::Success) {
     std::cerr << "Decomposition failed." << std::endl;
     csv_file << std::format("{}, {}, {}", "N/A", "N/A", "Decomposition failed.") << std::endl;
@@ -86,17 +97,26 @@ int solveMatrixMarket(const std::filesystem::path& path) {
     return -1;
   }
 
+  const auto decompTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+  const auto decompMem = solver.cholmod().memory_usage;
+  
+  std::cerr << std::format("Decomposition succeeded with in {} ms", decompTime) << std::endl;
+  csv_file << std::format("{}, {}, ", decompTime, decompMem);  
+
   Eigen::VectorXd b(A.rows()), x(A.rows()), xe(A.rows());
   x.setOnes();
   b = A * x;
 
+  std::cerr << std::format("Solving matrix...") << std::endl;
   start = std::chrono::high_resolution_clock::now();
   xe = solver.solve(b);
   end = std::chrono::high_resolution_clock::now();
 
   const auto solveTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
   const auto solveMem = 0;
-  csv_file << std::format("{}, {},", solveTime, solveMem);
+
+  std::cerr << std::format("Solve took {} ms and {} bytes", solveTime, solveMem) << std::endl;
+  csv_file << std::format("{}, {}, ", solveTime, solveMem);
 
   if (solver.info() != Eigen::Success) {
     std::cerr << "Solving failed." << std::endl;
@@ -104,7 +124,6 @@ int solveMatrixMarket(const std::filesystem::path& path) {
     csv_file.close();
     return -1;
   }
-
 
   // Valutazione dell'errore
   auto err = sqrt((x - xe).squaredNorm() / x.squaredNorm());
